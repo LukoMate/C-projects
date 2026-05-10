9ec36f5d9236aff319529aedb9cb29b6290d68fe
LukoMate <lukonicsmate@gmail.com>
2026-05-10 13:20:02 +0200

    Add files via upload

---
+++ move_vec.cpp
@@ -0,0 +1,32 @@
+#include <utility>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+struct Foo
+{
+	Foo() = default;
+	
+	Foo(vector<int>& v) : v1(v) { std::cout << "copy "; }
+	
+	Foo(vector<int>&& v) : v1(move(v)) { std::cout << "move "; }
+	vector <int> v1;
+};
+
+
+
+void FillVector(vector<int>& v) {}
+
+int main()
+{
+	vector<int> v {1,2,3,4};
+
+	Foo f1(move(v));  //v valid but unspecified state
+
+	Foo f2(v);
+	
+	v.push_back(42);
+	
+	v.clear();  //this is fine, because it doesn't depend on the state of v
+	//v.front();  //not fine
+}
